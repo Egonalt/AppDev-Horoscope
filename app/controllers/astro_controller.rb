5c8a1a62c0dad3002f4fde6c82c2d20a3d8b19e9
Egonalt <91084438+Egonalt@users.noreply.github.com>
2023-12-04 00:43:18 +0000

    add stock row

--- app/controllers/astro_controller.rb
+++ app/controllers/astro_controller.rb
@@ -1,9 +1,10 @@
-
-
 class AstroController < ApplicationController
   def home
+    session[:random_ticker] = random_stock_ticker
+
     render({ :template => "astro_templates/home" })
   end
+
   def show
     @astro_details = AstroApiService.fetch_astro_details(
       params[:day], 
@@ -16,6 +17,20 @@ class AstroController < ApplicationController
       params[:lon], 
       params[:tzone]
     )
+    @random_ticker = session[:random_ticker] || "N/A"
+
     render 'astro_templates/show'
+  end
+
+  private
+
+  def random_stock_ticker
+    tickers = File.readlines(Rails.root.join('lib', 'assets', 'stock_tickers.txt'))
+    selected_ticker = tickers.sample.strip
+    Rails.logger.info "Selected Stock Ticker: #{selected_ticker}"
+    selected_ticker
+  rescue StandardError => e
+    Rails.logger.error "Error reading stock tickers: #{e.message}"
+    "N/A"
   end
 end
